28b131be2785b80dc79f6f0de426bd898e46d98b
Janina Mass <janina.mass@gmail.com>
2022-07-28 15:30:28 +0200

    print dummy board

--- src/dinomite.rs
+++ src/dinomite.rs
@@ -1,23 +1,27 @@
 use rand::Rng;
 use std::collections::HashSet;
+use std::fmt::{Display, Formatter};
+use std::fmt::Write as _;
+
+#[derive(Debug, Hash, Clone, PartialEq, Eq)]
 struct Position(usize, usize);
 
 struct Dinomite {
     width: usize,
-    heigth: usize,
+    height: usize,
     seen: HashSet<Position>,
     dinos: HashSet<Position>,
-    flags: HashSet<Positioin>,
+    flags: HashSet<Position>,
     game_over: bool,
 }
 
 impl Dinomite {
     // Create a width x height field with num_dinos hidden dinos.
     // Upon starting no fields have been opened, no flags are set.
-    fn new(&self, width: usize, height: usize, num_dinos: usize) -> Self {
+    fn new(width: usize, height: usize, num_dinos: usize) -> Self {
         Dinomite {
             width,
-            heigth,
+            height,
             seen: HashSet::new(),
             dinos: {
                 let mut rng = rand::thread_rng();
@@ -30,5 +34,29 @@ impl Dinomite {
             flags: HashSet::new(),
             game_over: false,
         }
+    }
+}
+
+impl Display for Dinomite {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut board = String::new();
+        for d in &self.dinos{
+            write!(board, "🦖 at {:>1?} ", d)?;
+        }
+        write!(f,"{}", board)
+    }
+}
+
+
+#[cfg(test)]
+pub mod test{
+    use crate::dinomite::Dinomite;
+
+    #[test]
+    fn test_repr(){
+        let expected =5;
+        let dinomite = Dinomite::new(10, 10, expected);
+        print!("{}", dinomite);
+        assert_eq!(dinomite.dinos.len(), expected);
     }
 }
